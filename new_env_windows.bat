d2e7b40b05ad329daf393ffe813cb78c791fef7d
perevale <pere.sashe@ya.ru>
2023-03-26 17:42:27 +0200

    Update new_env_windows.bat

--- new_env_windows.bat
+++ new_env_windows.bat
@@ -1,5 +1,5 @@
 pip install virtualenv
 python -m virtualenv env
 env\Scripts\activate
-(echo pandas && echo  numpy && echo  scikit-learn && echo matplotlib) > requirements.txt
+(echo pandas && echo  numpy && echo  scikit-learn && echo matplotlib && echo ipykernel) > requirements.txt
 pip install -r requirements.txt
